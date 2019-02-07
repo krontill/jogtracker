fd43ed40d510af4246a7951210c661bdc3e67b22
krontill <till.work@gmail.com>
2019-02-07 23:42:16 +0300

    Test deploy app.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Test deploy app.
           </a>
         </header>
       </div>
