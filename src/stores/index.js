58e4839f19c1a14debb7855cd1261d06b3e88d49
krontill <till.work@gmail.com>
2019-02-09 16:25:25 +0300

    Fix in heroku - Can't resolve 'redux-devtools-extension/developmentOnly'.

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
 const localStorageMiddleware = ({getState}) => next => action => {
